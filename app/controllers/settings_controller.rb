b419abef3d17956e6fe507f5eade91436686b9ed
JBoydB <JaronB91@yahoo.com>
2017-12-08 00:27:46 -0700

    199 - Built out settings controller, added routes for settings edit and update, added logic for populating auto-text with existing values or empty when nil, settings update and create are folded in.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,16 +1,18 @@
 class SettingsController < ApplicationController
 
-
-  def create
-    redirect_to "/settings"
-  end
-
   def edit
-
+    @auto_text = current_admin.setting.auto_text || "" if current_admin.setting
+    render :edit
   end
 
   def update
-    redirect_to "/settings"
+    if current_admin.setting
+      current_admin.setting.update(auto_text: params[:auto_text])
+    else
+      Setting.create(admin_id: current_admin.id, auto_text: params[:auto_text])
+    end
+    flash[:success] = "Settings Saved!"
+    redirect_to "/"
   end
 
 end
